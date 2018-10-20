98c1a78ed49e520a2fd2efe4acaa32daa4072707
Finn Behrens <finn@trudeltiere.de>
2018-10-20 21:02:43 +0200

    add config foo
    add config file reading for port and ip

--- backend/bin/www.js
+++ backend/bin/www.js
@@ -7,13 +7,13 @@
 var app = require('../app');
 var debug = require('debug')('edushipbackend2:server');
 var http = require('http');
+var config = require('../config.json')
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+app.set('port', config.server.port);
 
 /**
  * Create HTTP server.
@@ -25,29 +25,9 @@ var server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
+server.listen(config.server.port, config.server.ip);
 server.on('error', onError);
 server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
 
 /**
  * Event listener for HTTP server "error" event.
@@ -59,8 +39,8 @@ function onError(error) {
   }
 
   var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+    ? 'Pipe ' + config.server.port
+    : 'Port ' + config.server.port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
